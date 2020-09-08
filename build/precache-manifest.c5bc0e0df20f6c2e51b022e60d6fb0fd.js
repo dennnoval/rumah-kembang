aed3bfe1af0b9cc01ab539fb80c57506aaa84db6
Noval Akbar <dennnoval@gmail.com>
2020-09-09 06:21:25 +0700

    Add build dir

---
+++ build/precache-manifest.c5bc0e0df20f6c2e51b022e60d6fb0fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64d62cf7e0158b0b6b945700876d298",
+    "url": "./index.html"
+  },
+  {
+    "revision": "206a989a7179f43c3eab",
+    "url": "./static/css/main.8d05f9ce.chunk.css"
+  },
+  {
+    "revision": "367fc813f90e908e375e",
+    "url": "./static/js/2.3e453cf1.chunk.js"
+  },
+  {
+    "revision": "fdd30b2ab44ae896589b798d411d5c2c",
+    "url": "./static/js/2.3e453cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206a989a7179f43c3eab",
+    "url": "./static/js/main.8a3b3584.chunk.js"
+  },
+  {
+    "revision": "554e522eee7c54baa763",
+    "url": "./static/js/runtime-main.ae26a880.js"
+  },
+  {
+    "revision": "183e42388e28516d6ad3f06c18a1772b",
+    "url": "./static/media/Nunito-Regular.183e4238.ttf"
+  },
+  {
+    "revision": "5851492545746b69470204dcbb4f8863",
+    "url": "./static/media/icon-arrow-left-short.58514925.svg"
+  },
+  {
+    "revision": "fbaaf51dad14fea884ecb22b0992957e",
+    "url": "./static/media/icon-cart-plus.fbaaf51d.svg"
+  },
+  {
+    "revision": "e3ce101b17546eb6ffdef67a54d57161",
+    "url": "./static/media/icon-cart.e3ce101b.svg"
+  },
+  {
+    "revision": "174272f35101da9f303c335ce3250141",
+    "url": "./static/media/icon-search.174272f3.svg"
+  },
+  {
+    "revision": "8290266a5fb402a34b96f890bbdb2d60",
+    "url": "./static/media/instagram.8290266a.png"
+  }
+]);
