dda396aec930754de45cee38d3be7df420a71256
dennnoval <dennnoval@gmail.com>
2022-05-12 21:15:26 +0700

    Update Category.js

--- reactjs/src/REST/Category.js
+++ reactjs/src/REST/Category.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 const Category = {
 	index : () => {
 		return axios.create({
-			baseURL: "https://rumah-kembang-api.herokuapp.com/api/v1/Category"
+			baseURL: "https://rumah-kembang-api.vercel.app/api/v1/Category"
 		})
 	},
 	getAllCategories : (token) => {
 		return axios({
 			method: "GET",
-			url: 'https://rumah-kembang-api.herokuapp.com/api/v1/Category/getAllCategories',
+			url: 'https://rumah-kembang-api.vercel.app/api/v1/Category/getAllCategories',
 			cancelToken: token
 		})
 	}
