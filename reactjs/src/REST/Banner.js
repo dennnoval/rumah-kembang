ed5cdacf4b4cf4bc16560c545b52d0a430080a63
dennnoval <dennnoval@gmail.com>
2022-05-12 21:15:24 +0700

    Update Banner.js

--- reactjs/src/REST/Banner.js
+++ reactjs/src/REST/Banner.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 const Banner = {
 	index : () => {
 		return axios.create({
-			baseURL: "https://rumah-kembang-api.herokuapp.com/api/v1/Carousel"
+			baseURL: "https://rumah-kembang-api.vercel.app/api/v1/Carousel"
 		})
 	},
 	getAllCarousels : (token) => {
 		return axios({
       method: "GET",
-      url: 'https://rumah-kembang-api.herokuapp.com/api/v1/Carousel/getAllCarousels',
+      url: 'https://rumah-kembang-api.vercel.app/api/v1/Carousel/getAllCarousels',
       cancelToken: token
     })
 	}
